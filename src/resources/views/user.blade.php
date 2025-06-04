67f3efdfb30b66ac6c0d72139fca4eca21818ba8
taie nobutaka <taie0424engineer@gmail.com>
2025-06-04 15:43:13 +0900

    tailwind cssからcssに変更

--- src/resources/views/user.blade.php
+++ src/resources/views/user.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>勤務一覧画面</title>
-    <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/user.css') }}">
 </head>
 <body>
     <div class="header">
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './resources/views/**/*.blade.php',
-    './resources/js/**/*.js',
-    './resources/css/**/*.css',
-    './public/**/*.html',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js",
+        "./resources/css/**/*.css",
+        "./public/**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
